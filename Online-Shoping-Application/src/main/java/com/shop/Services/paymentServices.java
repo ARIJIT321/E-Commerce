ebf6bef9782c8ead063100fa97442d16979dc4a4
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-02-04 19:48:35 +0530

    Update paymentServices.java

--- Online-Shoping-Application/src/main/java/com/shop/Services/paymentServices.java
+++ Online-Shoping-Application/src/main/java/com/shop/Services/paymentServices.java
@@ -11,15 +11,11 @@ import com.shop.Model.Payment;
 
 public interface paymentServices {
 
-	public Payment makePayment(Integer orderId,Integer cardId,Integer customerId,String key) 
-			throws LoginException,CustomerException,OrderException,PaymentException,CardException;
+	public Payment makePayment(Integer orderId,Integer cardId,Integer customerId,String key) throws LoginException,CustomerException,OrderException,PaymentException,CardException;
 	
-	public Payment viewPaymentDetailsById(Integer paymentId,Integer customerId,String key)
-			throws LoginException,CustomerException,OrderException,PaymentException;
+	public Payment viewPaymentDetailsById(Integer paymentId,Integer customerId,String key) throws LoginException,CustomerException,OrderException,PaymentException;
 	
-	public List<Payment> getAllPaymentByCustomer(Integer customerId,String key)
-			throws LoginException,CustomerException,OrderException,PaymentException;
+	public List<Payment> getAllPaymentByCustomer(Integer customerId,String key) throws LoginException,CustomerException,OrderException,PaymentException;
 	
-	public String cancelPayment(Integer payId,Integer customerId,String key)
-			throws LoginException,CustomerException,OrderException,PaymentException;
+	public String cancelPayment(Integer payId,Integer customerId,String key) throws LoginException,CustomerException,OrderException,PaymentException;
 }
