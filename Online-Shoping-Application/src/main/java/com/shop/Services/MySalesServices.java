e23962db35059288ccafa687b0133eada48a1d36
Arijit Bhatta <103993626+ARIJIT321@users.noreply.github.com>
2023-02-05 13:49:51 +0530

    Update MySalesServices.java

--- Online-Shoping-Application/src/main/java/com/shop/Services/MySalesServices.java
+++ Online-Shoping-Application/src/main/java/com/shop/Services/MySalesServices.java
@@ -11,8 +11,12 @@ import com.shop.Model.Order;
 public interface MySalesServices {
 
 	public List<Order> SalesForToday(Integer AdminId,String key) throws AdminException,LoginException,OrderException;
+	
 	public List<Order> SalesForWeek(Integer AdminId,String key) throws AdminException,LoginException,OrderException;
+	
 	public List<Order> SalesForMonth(Integer AdminId,String key) throws AdminException,LoginException,OrderException;
+	
 	public List<Order> SalesForYear(Integer AdminId,String key) throws AdminException,LoginException,OrderException;
+	
 	public List<Order> SalesBetweenDates(LocalDate l1,LocalDate l2,Integer AdminId,String key) throws AdminException,LoginException,OrderException;
 }
